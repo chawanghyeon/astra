cbb3b81f25e4ab1f2c4d1d875f03a099307985db
chawanghyeon <chawanghyeon@gmail.com>
2023-04-29 23:37:20 +0900

    add: tset version3

--- core/request.py
+++ core/request.py
@@ -1,22 +1,14 @@
 class Request:
     def __init__(self):
-        self.method = None
-        self.path = None
-        self.version = None
-        self.headers = {}
-        self.body = b""
+        self._method = None
+        self._version = None
+        self._path = None
+        self._headers = None
+        self._body = None
 
-    def on_url(self, url):
-        self.path = url
-
-    def on_header(self, name, value):
-        self.headers[name.decode()] = value.decode()
-
-    def on_body(self, body):
-        self.body += body
-
-    def on_message_complete(self):
-        pass  # Here you can add any post-processing needed after the message is complete
+    @property
+    def method(self):
+        return self._method.decode()
 
     def __repr__(self):
         return f"<Request method={self.method} path={self.path} version={self.version} headers={self.headers}>"
