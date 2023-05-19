4d441517268b361a0d1d6d0dcab568decc90a222
chawanghyeon <chawanghyeon@gmail.com>
2023-05-19 23:56:30 +0900

    add: enter_context and exit context

--- middleware/asyncexitstack.py
+++ middleware/asyncexitstack.py
@@ -1,21 +1,30 @@
-import asyncio
 from contextlib import AsyncExitStack
 from core.request import Request
 from core.response import Response
-from typing import Callable, Optional, Tuple
-from .database import AsyncDBConnection  # Hypothetical database connection class
+from typing import Callable
+from middleware.base import BaseMiddleware
 
 
 class AsyncExitStackMiddleware(BaseMiddleware):
+    async def enter_context(self, request: Request, stack: AsyncExitStack) -> None:
+        """
+        Enter the async context for this request.
+        This is where you should manage your resources.
+        """
+        db_conn = await stack.enter_async_context(AsyncDBConnection())
+        request.db_conn = db_conn
+
+    async def exit_context(self, request: Request, stack: AsyncExitStack) -> None:
+        """
+        Exit the async context for this request.
+        Any cleanup required when the request is done should be done here.
+        """
+        # Cleanup can be done here if necessary
+
     async def __call__(self, handler: Callable, request: Request) -> Response:
         async with AsyncExitStack() as stack:
-            # Suppose we have a database connection to manage
-            db_conn = await stack.enter_async_context(AsyncDBConnection())
+            await self.enter_context(request, stack)
 
-            # Add the connection to the request, so it's accessible in the handler
-            request.db_conn = db_conn
-
-            # Now proceed with request processing and handler invocation
             request, response = await self.process_request(request)
 
             if not response:
@@ -23,5 +32,5 @@ class AsyncExitStackMiddleware(BaseMiddleware):
 
             response = await self.process_response(request, response)
 
-            # The AsyncExitStack will automatically close the DB connection here
+            await self.exit_context(request, stack)
         return response
