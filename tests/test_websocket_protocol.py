59d053218df833dfea4133da285df4a4bcb9bcbb
chawanghyeon <chawanghyeon@gmail.com>
2023-06-23 00:49:38 +0900

    test: websocket_protocol

--- tests/test_websocket_protocol.py
+++ tests/test_websocket_protocol.py
@@ -1,28 +1,17 @@
-import asyncio
 import pytest
-import websockets
+from unittest.mock import AsyncMock
 from core.websocket_protocol import WebSocketProtocol  # replace with the actual import
 
+
+class MockApp:
+    class MockRouter:
+        dispatch_websocket = AsyncMock()
+
+    router = MockRouter()
+
+
 @pytest.mark.asyncio
-async def test_websocket_protocol():
-    # Define a mock app with a router that handles websocket messages
-    class MockApp:
-        class MockRouter:
-            async def dispatch_websocket(self, path, message):
-                assert path == '/test'
-                assert message == 'test message'
-        router = MockRouter()
-
-    # Create the protocol
+async def test_process_message():
     protocol = WebSocketProtocol(MockApp(), None, None)
-
-    # Run the server in the background
-    server = await asyncio.start_server(protocol.handler, 'localhost', 8765)
-
-    # Use the websockets client to connect to the server and send a message
-    async with websockets.connect('ws://localhost:8765/test') as websocket:
-        await websocket.send('test message')
-
-    # Close the server
-    server.close()
-    await server.wait_closed()
+    await protocol.process_message("/test", "test message")
+    MockApp.router.dispatch_websocket.assert_called_once_with("/test", "test message")
