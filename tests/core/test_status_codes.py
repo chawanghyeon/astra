01bdb6ab2686cef0a806e4a428ca96e8476d2924
chawanghyeon <chawanghyeon@gmail.com>
2023-07-04 23:43:18 +0900

    test: add test

--- tests/core/test_status_codes.py
+++ tests/core/test_status_codes.py
@@ -1,23 +1,23 @@
 import pytest
 
-from core import Status
+from core import status
 
 
-def test_get_status_reason():
-    assert Status.get_status_reason(Status.OK) == "OK"
-    assert Status.get_status_reason(Status.NOT_FOUND) == "Not Found"
-    assert Status.get_status_reason(Status.INTERNAL_SERVER_ERROR) == "Internal Server Error"
-    assert Status.get_status_reason(999) == "Unknown Status"
+def test_get_status_reason() -> None:
+    assert status.get_status_reason(status.OK) == "OK"
+    assert status.get_status_reason(status.NOT_FOUND) == "Not Found"
+    assert status.get_status_reason(status.INTERNAL_SERVER_ERROR) == "Internal Server Error"
+    assert status.get_status_reason(999) == "Unknown Status"
 
 
 @pytest.mark.parametrize(
     "status_code,expected_reason",
     [
-        (Status.OK, "OK"),
-        (Status.NOT_FOUND, "Not Found"),
-        (Status.INTERNAL_SERVER_ERROR, "Internal Server Error"),
+        (status.OK, "OK"),
+        (status.NOT_FOUND, "Not Found"),
+        (status.INTERNAL_SERVER_ERROR, "Internal Server Error"),
         (999, "Unknown Status"),
     ],
 )
-def test_get_status_reason_parametrized(status_code, expected_reason):
-    assert Status.get_status_reason(status_code) == expected_reason
+def test_get_status_reason_parametrized(status_code: int, expected_reason: str) -> None:
+    assert status.get_status_reason(status_code) == expected_reason
